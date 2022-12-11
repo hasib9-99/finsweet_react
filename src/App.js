7c379d11262acaf093061f4aeccada3e84c4f3fa
hasib9-99 <abuhasibg@gmail.com>
2022-12-12 00:16:31 +0600

    routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import './App.css';
-import Navbar from './componats/navbar/Navbar';
-import Banner from './componats/banner/Banner';
-import Work from './componats/work/Work';
-import Project from './componats/project/Project'
-import Features from './componats/features/Features';
-import About from './componats/About/About';
-import Question from './componats/question/Question';
-import Building from './componats/building/Building';
-import Blog from './componats/blog/Blog';
-import Footer from './componats/footer/Footer';
+import Home from './Home';
+// import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Banner/>
-    <Work/>
-    <Project/>
-    <Features/>
-    <About/>
-    <Question/>
-    <Building/>
-    <Blog/>
-    <Footer/>
-
+    <Home/>
     </>
   );
 }
